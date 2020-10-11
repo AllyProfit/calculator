a39a77571b05217978f8f7da048d4720b09c5923
unknown <allyprofit@gmail.com>
2020-10-11 15:38:40 +0300

    Done motherfuckers

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -2,23 +2,22 @@ const input = document.getElementById('result')
 const deleteString = document.getElementById('deleteString');
 const deleteAll = document.getElementById('deleteAll');
 const deleteOne = document.getElementById('deleteOne');
-const divide = document.getElementById('divide');
-const multiply = document.getElementById('multiply');
-const minus = document.getElementById('minus');
-const plus = document.getElementById('plus');
 const plusMinus = document.getElementById('plusMinus');
 const dot = document.getElementById('dot');
 const equal = document.getElementById('equal');
 const output = document.getElementById('resultNumber');
 const numbers = document.getElementsByClassName('calculator__buttons__number')
-console.log(numbers)
+const actions = document.getElementsByClassName('calculator__buttons__action')
 const handlePlusMinus = () => {
     if (result.value != '0')  {
         result.value = -result.value
     }
 }
 const handleDot = () => {
-    result.value +='.'
+    
+    if (!result.value.includes('.')) {
+        result.value +='.'
+    }
 }
 const number = (num) => { 
     if (result.value == '0'){
@@ -28,14 +27,13 @@ const number = (num) => {
         result.value +=num
     }
 }
-
 const action = (element) => { 
-    
+    console.log(element)
     if (result.value !== '0') {
         const act = element.innerHTML 
         result.value += ` ${act}`;
         output.innerHTML+= result.value;
-        result.value = '';
+        result.value = '0';
     }
 }
 const deleteAction = (index) => {
@@ -55,7 +53,6 @@ const deleteAction = (index) => {
       }
 }
 const calc = () =>{
-    
     const value = output.innerHTML+ result.value;
     const resultValue = eval(value)
     if (resultValue.toString().length<=10){
@@ -68,22 +65,19 @@ const calc = () =>{
     }
     output.innerHTML = '';
 }
-const arrayTwo = [plus, minus, multiply,divide]
-for (let j = 0; j< arrayTwo.length; j++)
-{
-    arrayTwo[j].addEventListener('click',()=> action(arrayTwo[j]))
-}
 const arrayThree = [deleteString,deleteAll,deleteOne]
 for (let i = 0; i< arrayThree.length;i++)
 {
     arrayThree[i].addEventListener('click',()=> deleteAction(i))
 }
-
 equal.addEventListener('click',() => calc() )
 dot.addEventListener('click',()=> handleDot())
 plusMinus.addEventListener('click',()=> handlePlusMinus())
 for (let i = 0; i< numbers.length; i++){
     numbers[i].addEventListener('click',()=> number(numbers[i].innerHTML))
+}
+for ( let i = 0; i< actions.length; i++){
+    actions[i].addEventListener('click',()=> action(actions[i]))
 }
 document.addEventListener('keyup',(event) => {
     for (let i = 0; i < numbers.length;i++){
@@ -93,6 +87,22 @@ document.addEventListener('keyup',(event) => {
             number(btn)
         }
     }
-console.log(event.key)
+    for (let i = 0; i < actions.length;i++){
+        const act = actions[i].innerHTML;
+    
+        if (event.key== act){
+            action(actions[i])
+        }
+    }
+    if (event.key == '.') {
+        handleDot()
+    }
+    if (event.key == '='|| event.key == 'Enter'){
+        calc()
+    }
+    if (event.key == 'Backspace'){
+        deleteAction(2)
+    }
 })
+
 
