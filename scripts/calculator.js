250607d512df5bbf0cc51af5f9ff2cc8c952d0c7
unknown <allyprofit@gmail.com>
2020-10-11 13:17:41 +0300

    calculator is almost done

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -20,24 +20,40 @@ const dot = document.getElementById('dot');
 const equal = document.getElementById('equal');
 const zero = document.getElementById('zero');
 const output = document.getElementById('resultNumber');
+const handlePlusMinus = () => {
+    if (result.value != '0')  {
+        result.value = -result.value
+    }
+}
+const handleDot = () => {
+    result.value +='.'
+}
 const number = (num) => { 
-    result.value +=num;
+    if (result.value == '0'){
+        result.value = num
+    }
+    else {
+        result.value +=num
+    }
 }
 
 const action = (element) => { 
-    const act = element.innerHTML 
-    result.value += ` ${act}`;
-    output.innerHTML+= result.value;
-    result.value = '';
+    
+    if (result.value !== '0') {
+        const act = element.innerHTML 
+        result.value += ` ${act}`;
+        output.innerHTML+= result.value;
+        result.value = '';
+    }
 }
 const deleteAction = (index) => {
     switch(index) {
         case 0:  
-          result.value = ''
+          result.value = '0'
           break 
       
         case 1:  
-          result.value = ''
+          result.value = '0'
           output.innerHTML = ''
           break
         case 2:
@@ -48,16 +64,24 @@ const deleteAction = (index) => {
 }
 const calc = () =>{
     
-const value = output.innerHTML+ result.value;
-result.value = eval(value)
-output.innerHTML = '';
+    const value = output.innerHTML+ result.value;
+    const resultValue = eval(value)
+    if (resultValue.toString().length<=10){
+        result.value = resultValue
+    }
+    else {
+        const trimmedResult = Math.round(resultValue*10e9)/10e9
+        console.log(trimmedResult, resultNumber)
+        result.value = trimmedResult
+    }
+    output.innerHTML = '';
 }
 const array = [
     zero, one, two, three, four, five, six, seven, eight, nine
 ]
 for (let i = 0; i< array.length; i++)
 {
-array[i].addEventListener('click',()=> number(i))
+    array[i].addEventListener('click',()=> number(i))
 };
 const arrayTwo = [plus, minus, multiply,divide]
 for (let j = 0; j< arrayTwo.length; j++)
@@ -71,3 +95,5 @@ for (let i = 0; i< arrayThree.length;i++)
 }
 
 equal.addEventListener('click',() => calc() )
+dot.addEventListener('click',()=> handleDot())
+plusMinus.addEventListener('click',()=> handlePlusMinus())
